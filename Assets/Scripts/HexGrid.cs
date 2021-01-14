fefd79702f29c0c5ebf2136683f3750a497ec1fa
fuli.wang <fuli.wang@modooplay.com>
2021-01-14 18:20:10 +0800

    add int. 六角形

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+    public HexCell cellPrefab;
+    public Text cellLablePrefab;
+    Canvas gridCanvas;
+    HexMesh hexMesh;
+    HexCell[] cells;
+
+    public Color defaultColor = Color.blue;
+    public Color touchColor = Color.yellow;
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+        cells = new HexCell[width * height];
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            HandleInput();
+        }
+    }
+
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            TouchCell(hit.point);
+        }
+    }
+
+    void TouchCell(Vector3 position)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = touchColor;
+        hexMesh.Triangulate(cells);
+        Debug.Log("touch  at " + coordinates.ToString());
+    }
+
+    public void ColorCell(Vector3 position, Color color)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = color;
+        hexMesh.Triangulate(cells);
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+        HexCell cell = cells[i] = GameObject.Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinations = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
+
+        Text lable = GameObject.Instantiate<Text>(cellLablePrefab);
+        lable.rectTransform.SetParent(gridCanvas.transform, false);
+        lable.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        lable.text = cell.coordinations.ToStringOnSeparateLines();
+    }
+
+}
---
+++ Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public static class HexMetrics
+{
+    public const float outerRadius = 10f;
+    public static float innerRadius = outerRadius * 0.866025404f;
+
+    public readonly static Vector3[] corners = new Vector3[]
+    {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0,0,outerRadius),
+    };
+
+}
